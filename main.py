d4b82f0642f1ebc1a0b5998aba082032f7f12808
Kathryn Rieb <kanrieb@gmail.com>
2020-03-10 16:03:23 -0700

    adding vocab

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import time
+
 print("Welcome to Hangman.")
 print ("Type Automatic to play the computer, or Manual to enter your own word:")
 mode = input()
+print()
 
-if mode == 'manual' or mode == 'Manual':
-    print ("Playing Manual mode. Enter your word:")
-elif mode == 'automatic' or mode == 'Automatic':
+if mode == 'automatic' or mode == 'Automatic':
     print ("Playing Automatic mode.")
-    print ("Generating word...")
+    time.sleep(1)
+    print ("Choosing word...")
+    time.sleep(2)
+    print ("Word has been chosen:")
+elif mode == 'manual' or mode == 'Manual':
+    print ("Playing Manual mode. Enter your word:")
+    manualWord = input()
+else:
+    print("Invalid input. Please try again") 
