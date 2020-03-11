bd44e632f2b1ee5fa198324ed46fe5385a610974
Kathryn Rieb <kanrieb@gmail.com>
2020-03-11 11:49:46 -0700

    Added error handling and fixed random choice

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hangman
+This is a simple hangman game with the sole intention of helping me learn and practice Python.
+There are two text files included, with a list of words that the automatic mode can choose from.
+You can play against the computer (automatic) or have someone else input a word for you.
+Currently only designed for the command line, future versions may have a proper game interface.
--- main.py
+++ main.py
@@ -1,37 +1,54 @@
 import time
 import random
 
+def validatorFunc(funcInput, stage):
+    if stage == 'mode':
+        if funcInput == 'automatic' or funcInput == 'Automatic' or funcInput == 'manual' or funcInput == 'Manual':
+            return funcInput
+        else:
+            print ()
+            print("Invalid input. Please try again:")
+            funcInput = input()
+            return validatorFunc(funcInput, 'mode')
+    elif stage == 'difficulty':
+        if funcInput == 'easy' or funcInput == 'Easy' or funcInput == 'Difficult' or funcInput == 'difficult':
+            return funcInput
+        else:
+            print ()
+            print("Invalid input. Please try again:")
+            funcInput = input()
+            return validatorFunc(funcInput, 'difficulty')
+
+def random_line(afile):
+    line = next(afile)
+    for num, aline in enumerate(afile, 2):
+      if random.randrange(num): continue
+      line = aline
+    return line
+
+
 print()
 print("Welcome to Hangman.")
 print ("Type Automatic to play the computer, or Manual to enter your own word:")
-mode = input()
+inputMode = input()
+
+valMode = validatorFunc(inputMode, 'mode')
 print()
 
-if mode == 'automatic' or mode == 'Automatic':
+if valMode == 'automatic' or valMode == 'Automatic':
     print ("Playing Automatic mode. Enter level (Easy or Difficult):")
     level = input()
+    valLevel = validatorFunc(level, 'difficulty')
     print()
 
     print ("Choosing word...")
-    f = open("%s.txt" %(level.lower()), "r")
-    lineCount = 0
-    for x in f:
-        lineCount +=1
-    f.seek(0)
-    random.seed(10)
-    rand = random.randint(1,lineCount)
-    counter = 0
-    for y in f:
-        counter +=1
-        if counter == rand:
-            word = y
+    f = open("%s.txt" %(valLevel.lower()), "r")
+    word = random_line(f)
     f.close()
     time.sleep(2)
 
     print ("Word has been chosen:")
     print(word)
-elif mode == 'manual' or mode == 'Manual':
+elif valMode == 'manual' or valMode == 'Manual':
     print ("Playing Manual mode. Enter your word:")
-    manualWord = input()
-else:
-    print("Invalid input. Please try again") 
+    word = input()
