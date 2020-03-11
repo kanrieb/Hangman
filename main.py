7a07a8d26d0f28e58240ec704578ac87ce9dd6c9
Kathryn Rieb <kanrieb@gmail.com>
2020-03-10 23:35:12 -0700

    Updated automatic functionality

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import time
+import random
 
+print()
 print("Welcome to Hangman.")
 print ("Type Automatic to play the computer, or Manual to enter your own word:")
 mode = input()
 print()
 
 if mode == 'automatic' or mode == 'Automatic':
-    print ("Playing Automatic mode.")
-    time.sleep(1)
+    print ("Playing Automatic mode. Enter level (Easy or Difficult):")
+    level = input()
+    print()
+
     print ("Choosing word...")
+    f = open("%s.txt" %(level.lower()), "r")
+    lineCount = 0
+    for x in f:
+        lineCount +=1
+    f.seek(0)
+    random.seed(10)
+    rand = random.randint(1,lineCount)
+    counter = 0
+    for y in f:
+        counter +=1
+        if counter == rand:
+            word = y
+    f.close()
     time.sleep(2)
+
     print ("Word has been chosen:")
+    print(word)
 elif mode == 'manual' or mode == 'Manual':
     print ("Playing Manual mode. Enter your word:")
     manualWord = input()
